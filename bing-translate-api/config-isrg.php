3edbf587797583da84c30bb0baabbf498184768f
Isrg Rajan <isrgrajan@outlook.com>
2018-11-06 18:23:25 +0530

    Create config-isrg.php

---
+++ bing-translate-api/config-isrg.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ISRG PHP API FOR MICROSOFT AZURE TRANSLATE TEXT
+Copyrighted 20018 Isrg Rajan (www.isrgrajan.com)
+1. API Settings, you can generate the API from: https://azure.microsoft.com/en-us/services/cognitive-services/translator-text-api/
+2. Get the list of supported languages from : https://docs.microsoft.com/en-us/azure/cognitive-services/translator/language-support
+*/
+
+define("default_translate_from_lang","en"); // en stands for English
+define("default_translate_to_lang","hi"); // hi stands for Hindi
+define("Azure_Translate_Text_API","ENTER_YOUR_TRANSLATE_TEXT_API_HERE");
